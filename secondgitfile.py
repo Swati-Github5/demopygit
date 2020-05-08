2707bb6341daed3279be229acf287f8dae50ebcd
Swati Unhale <swatiunhale5@gmail.com>
2020-05-08 16:04:35 +0530

    added two more lines to print

---
+++ secondgitfile.py
@@ -0,0 +1,7 @@
+print("This is my 2nd file in git repository addition")
+
+for i in range(5):
+    print("I did it")
+
+for i in range(2):
+    print("I did the small chnages and tring to push on got local depositiry")
